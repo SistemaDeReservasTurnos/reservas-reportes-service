d61321ac535ec74c60f9667c678f01dbf3105675
JAlexis1 <ruizquicenojhonalexis@gmail.com>
2025-12-04 21:55:16 -0500

    configuracion inicial con dependencias

---
+++ reservas-reportes-service/src/main/java/com/servicio/reservas/reportes/ReservasReportesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.servicio.reservas.reportes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasReportesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservasReportesServiceApplication.class, args);
+    }
+
+}
---
+++ reservas-reportes-service/src/test/java/com/servicio/reservas/reportes/ReservasReportesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicio.reservas.reportes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasReportesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
